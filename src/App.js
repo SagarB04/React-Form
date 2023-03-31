9d64ac26a30166a2c7bb27bc365320c6c2a728c2
SagarB04 <barmansagar01@gmail.com>
2023-04-01 04:39:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Form from './components/form.jsx'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="static">
+        <span>Learn to code by watching others</span>
+        <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+      </div>
+      <div>
+        <Form/>
+      </div>
     </div>
   );
 }
---
+++ src/components/form.jsx
@@ -0,0 +1,133 @@
+import React, { useState, useEffect } from 'react'
+import Select from 'react-select'
+
+export default function Form() {
+
+    let option = [
+        { value: 'HTML', label: "HTML" },
+        { value: 'CSS', label: "CSS" },
+        { value: 'JS', label: "JS" },
+        { value: 'REACT', label: "REACT" },
+        { value: 'JAVA', label: "JAVA" },
+        { value: 'C/C++', label: "C/C++" }
+    ]
+
+    const [arr, setArr] = useState([]);
+
+    const [active, setActive] = useState(true)
+
+    const [message, setMessage] = useState(
+        "Try it free 7 days then 180/mo. thereafter"
+    )
+
+    const [formValue, setFormValue] = useState(
+        {
+            name: "",
+            email: "",
+            password: ""
+        }
+    )
+
+    function handleChange(e) {
+        setFormValue({ ...formValue, [e.target.name]: e.target.value })
+    }
+    function isValidEmail(email) {
+        return /\S+@\S+\.\S+/.test(email);
+      }
+    useEffect(() => {
+        if (formValue.name && isValidEmail(formValue.email) && formValue.password.length >= 6 && arr.length > 0) {
+            setActive(false)
+        }
+        else setActive(true)
+
+    }, [formValue, arr])
+
+    function handleSkill(e) {
+        let value = e.value;
+        if (value && !arr.includes(value)) {
+            setArr([...arr, value])
+        }
+    }
+
+    function handleRemoveSkill(skillToRemove) {
+        setArr((prevSkills) =>
+            prevSkills.filter((skill) => skill !== skillToRemove)
+        )
+    }
+
+    function buttonClick() {
+        setMessage("You have successfully subscribed to our plan ✔")
+        setFormValue({
+            name: "",
+            email: "",
+            password: "",
+        })
+        setActive(true);
+        setArr([])
+        setTimeout(() => {
+            setMessage("Try it free 7 days then 180/mo. thereafter")
+        }, 2000);
+
+    }
+
+    return (
+
+        <div className="dynamic">
+            <div className='message'>
+                <p>{message}</p>
+            </div>
+            <form className='form-s'>
+
+                <input
+                    name='name'
+                    type="text"
+                    placeholder='Name'
+                    onChange={handleChange}
+                    value={formValue.name}
+                />
+                <input
+                    name='email'
+                    type="email"
+                    placeholder='Email Address (example@gmail.com)'
+                    onChange={handleChange}
+                    value={formValue.email}
+                />
+                <input
+                    name='password'
+                    type="password"
+                    placeholder='Password (min 6 characters)'
+                    onChange={handleChange}
+                    value={formValue.password}
+                />
+
+                <div className='select'>
+                    <Select
+                        className='select-s'
+                        placeholder={<div className="select-placeholder-text">Choose your skills</div>}
+                        options={option}
+                        onChange={handleSkill}
+                        isOptionDisabled={(option) => option.isdisabled}
+                    />
+                </div>
+
+                <div className='skill'>
+                    {
+                        arr.map((i) => {
+                            return (
+                                <div className='skill-s'>
+                                    {i}
+                                    <span onClick={() => handleRemoveSkill(i)}>X</span>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
+                <button disabled={active} onClick={buttonClick} type="button">CLAIM YOUR FREE TRIAL</button>
+
+                <div className="txt">By clicking the button you are agreeing to our <span>Terms and Services</span></div>
+            </form>
+
+        </div>
+    )
+}
